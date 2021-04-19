c5a3957812cbd17f64059435cf9e21ac6c387a5a
elivelton gustavo da silva <elivelton.gustavo34@gmail.com>
2021-04-19 10:11:12 -0300

    first commit

---
+++ src/main/java/com/elivelton/javakubernates/JavaKubernatesApplication.java
@@ -0,0 +1,13 @@
+package com.elivelton.javakubernates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKubernatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKubernatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elivelton/javakubernates/JavaKubernatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elivelton.javakubernates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKubernatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
